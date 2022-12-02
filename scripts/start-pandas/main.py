6fda5b6764172e4109887994baf28e7306d38f60
Dieisson Martins <dieisson.martins.santos@gmail.com>
2022-12-02 19:49:23 -0300

    testes com Pandas()

--- scripts/start-pandas/main.py
+++ scripts/start-pandas/main.py
@@ -9,9 +9,30 @@ logging.info('processing script test pandas')
 logging.info('-----------------------------\n')
 
 my_stores = {
-    'stores': ['Supermercado BH', 'Supermercado NC'],
-    'counters': [78, 8]
+    'stores': [
+        'Supermercado BH',
+        'Supermercado NC',
+        'Supermercado 12',
+        'Supermercado 134',
+        'Supermercado 34',
+        'Supermercado 67'
+    ],
+    'counters': [
+        78,
+        8,
+        23,
+        86,
+        89,
+        10
+    ]
 }
 
 frame = pandas.DataFrame(my_stores)
+
 print(frame)
+logging.info('-----------------------------\n')
+print(frame['stores'])
+logging.info('-----------------------------\n')
+print(frame.describe())
+logging.info('-----------------------------\n')
+print(frame.sort_values(by='counters'))
